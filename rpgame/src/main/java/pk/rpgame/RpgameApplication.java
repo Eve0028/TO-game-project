c5a127c8a067045f69c437db22401112bcd69c3b
Eve0028 <sylwia14082@gmail.com>
2022-12-09 10:28:33 +0100

    Add project structure

---
+++ rpgame/src/main/java/pk/rpgame/RpgameApplication.java
@@ -0,0 +1,13 @@
+package pk.rpgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgameApplication.class, args);
+	}
+
+}
