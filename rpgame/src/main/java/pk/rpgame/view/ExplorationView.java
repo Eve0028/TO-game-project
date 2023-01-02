a595521e5b4e5f52b87ae9cc4918f4185cca2621
Kacper Ledwon <you@example.com>
2023-01-02 16:28:48 +0100

    create exploration view class

---
+++ rpgame/src/main/java/pk/rpgame/view/ExplorationView.java
@@ -0,0 +1,71 @@
+package pk.rpgame.view;
+
+import pk.rpgame.model.Room;
+import java.util.List;
+import java.util.Scanner;
+
+interface MenuClickListener {
+  void onActionClick(int num);
+}
+
+enum PickUpItems {
+  PICK_UP,
+  DONT_PICK_UP
+}
+
+public class ExplorationView {
+
+  private MenuClickListener listener;
+
+  public void printRoomDescription(Room room) {
+    System.out.println("Znajdujesz się w pomieszczeniu " + room.getName());
+    if (room.getCreatures().isEmpty()) {
+      System.out.println("W tym pokoju jesteś bezpieczny. Nie ma tu żadnych potworów.");
+    } else {
+      System.out.println("W tym pokoju znajdują się potwory: " + room.getCreatures());
+      System.out.println("Za chwilę nastąpi walka!");
+    }
+  }
+
+  public void setListener(MenuClickListener listener) {
+    this.listener = listener;
+  }
+
+  private int getAction() {
+    Scanner keyboard = new Scanner(System.in);
+    int action = keyboard.nextInt();
+    keyboard.close();
+    return action;
+  }
+
+  public void printNearestRooms(List<Room> nearestRooms) {
+    System.out.println("Możliwe pomieszczenia, do których możesz się udać: " + nearestRooms);
+  }
+
+  public PickUpItems pickUpItems() {
+    System.out.println(
+        "Możesz podnieść znalezione przedmioty. Wybierz '1' aby je podnieść lub '0' aby nic nie robić.");
+    System.out.println(
+        "Podniesienie pancerza lub broni spowoduje upuszczenie aktualnie noszonych przedmiotów z tej kategorii");
+    int action = getAction();
+    while (true) {
+      if (action == 1) {
+        return PickUpItems.PICK_UP;
+      } else if (action == 0) {
+        return PickUpItems.DONT_PICK_UP;
+      } else {
+        System.out.println("Wybierz właściwą cyfrę!");
+      }
+      action = getAction();
+    }
+  }
+
+  public void showMenu() {
+    System.out.println("1. Eksploruj pomieszczenie");
+    System.out.println("2. Przejdź do następnego pokoju");
+    System.out.println("3. Pokaż mapę");
+    System.out.println("4. Pokaż menu");
+    int action = getAction();
+    listener.onActionClick(action);
+  }
+}
