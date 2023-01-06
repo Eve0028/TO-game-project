78f34f66ecc2a16608a0186edf14e59d5e1ed140
Kacper Ledwon <you@example.com>
2023-01-06 16:47:38 +0100

    add state machine for picking controllers

---
+++ rpgame/src/main/java/pk/rpgame/controller/ExplorationController.java
@@ -0,0 +1,5 @@
+package pk.rpgame.controller;
+
+public class ExplorationController implements IController {
+
+}
---
+++ rpgame/src/main/java/pk/rpgame/controller/IController.java
@@ -0,0 +1,5 @@
+package pk.rpgame.controller;
+
+public interface IController {
+
+}
