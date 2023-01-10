2722b4bd82c1e06d0362a46a0efb91e3bcf8f053
Kacper Ledwon <you@example.com>
2023-01-10 20:35:49 +0100

    update writing saves to file

--- rpgame/src/main/java/pk/rpgame/model/singleton/AccessSaveFile.java
+++ rpgame/src/main/java/pk/rpgame/model/singleton/AccessSaveFile.java
@@ -10,68 +10,63 @@ import java.util.List;
 import java.util.Scanner;
 
 public class AccessSaveFile {
-    private static AccessSaveFile instance=null;
+    private static AccessSaveFile instance = null;
     File file;
 
-    private AccessSaveFile(){
-        file=new File("data.txt");
+    private AccessSaveFile() {
+        file = new File("data.txt");
     }
 
-    public static AccessSaveFile getInstance(){
-        if(instance==null){
-            instance=new AccessSaveFile();
+    public static AccessSaveFile getInstance() {
+        if (instance == null) {
+            instance = new AccessSaveFile();
         }
 
         return instance;
     }
 
-    public void saveData(String name, Double HP, Double strength,List<Item>  items){
-        if(!file.exists()){
+    public void saveData(String name, Double HP, Double strength, List<Item> items) {
+        if (!file.exists()) {
             try {
                 file.createNewFile();
-            }catch (Exception e){
+            } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
         }
 
-        if(file.canWrite()) {
+        if (file.canWrite()) {
             try {
-                FileWriter fw = new FileWriter(file, false);
-                BufferedWriter writer= new BufferedWriter(fw);
+                FileWriter fw = new FileWriter(file, true);
+                BufferedWriter writer = new BufferedWriter(fw);
 
+                writer.write("Hero name: " + name + "\n");
+                writer.write("Hero HP was equal: " + HP.toString() + "\n");
+                writer.write("Hero strength was equal: " + strength.toString() + "\n");
+                writer.write("\nIn his inventory was: \n");
 
-                 writer.write("!!!!Hall of fame!!!!\n");
-                 writer.write("Hero name: " +name+"\n");
-                 writer.write("Hero HP was equal: "+HP.toString()+"\n");
-                 writer.write("Hero strength was equal: " +strength.toString()+"\n");
-                 writer.write("\nIn his inventory was: \n");
-
-                for (Item item:
-                     items) {
-                    writer.write("Item name: "+item.getName());
+                for (Item item : items) {
+                    writer.write("Item name: " + item.getName() + "\n");
 
                 }
 
-                //zamknięcie pliku
+                writer.write("\n====================\n\n");
                 writer.close();
                 fw.close();
-            }catch (Exception e){
+            } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
         }
     }
 
-
-    public List<String> loadData(){
-        try{
-            List<String> fileList= Files.readAllLines(Paths.get("data.txt"));
+    public List<String> loadData() {
+        try {
+            List<String> fileList = Files.readAllLines(Paths.get("data.txt"));
             return fileList;
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
         return null;
     }
-
 
 }
